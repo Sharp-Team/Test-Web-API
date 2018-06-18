d874de33c869e709eafc8cb783f708cc5d0d8e31
Loser Teach <jetaimefrc@gmail.com>
2018-06-18 23:19:10 +0700

    Test Web API
    Done simple web API

---
+++ WebApplication/WebApplication/Data/TodoContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Data
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        {
+
+        }
+        private DbSet<ToDo> toDo;
+
+        public DbSet<ToDo> ToDo { get => toDo; set => toDo = value; }
+    }
+}
